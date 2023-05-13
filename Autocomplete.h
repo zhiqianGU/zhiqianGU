24539778cfbd0780b2e46b4d50f8c0a08ad9a8d4
qian <2635592130@qq.com>
2023-05-13 23:12:44 +0930

    1

--- Autocomplete.h
+++ Autocomplete.h
@@ -2,11 +2,12 @@
 #define AUTOCOMPLETE_H
 #include<vector>
 #include<iostream>
-class Autocomplete{
+#include<string>
+class Autocomplete {
 public:
 	std::vector<string> getSuggestions(std::string partialWord)
 		void insert(std::string word);
 private:
 	std::vector<std::string> words;
-}
+};
 #endif
