830bb073b1ca8409530c65298ccd7c744a611339
zhiqianGU <104746268+zhiqianGU@users.noreply.github.com>
2023-05-13 22:58:08 +0930

    Add files via upload

---
+++ PrefixMatcher.cpp
@@ -0,0 +1,28 @@
+#include"PrefixMatcher.h"
+int PrefixMatcher::selectRouter(std::string networkAddress) {
+	int longest_match = 0;
+	int routerNumber = 0;
+	for (int i =0 ; i < addresses.length(); i++) {
+		int min = min(networkAddress.length(), addresses.length());
+		int match_length =0;
+		for (int j = 0; j < min; j++)
+		{
+			if (addresses[i][j] == networkAddress[j]) {
+				match_length++;
+			}
+			else {
+				i = min;
+			}
+		}
+		if (match_length > longest_match) {
+			longest_match = match_length;
+			routerNumber = i;
+		}
+	}
+	return routerNumber;
+}
+void PrefixMatcher::insert(std::string address, int routerNumber)
+{
+	addresses.add(address);
+	numbers.add(routerNumber);
+}
