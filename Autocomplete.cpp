8ddb0ab68f67a978102cdf0a9d73339da36b049b
qian <2635592130@qq.com>
2023-05-13 23:31:32 +0930

    1

--- Autocomplete.cpp
+++ Autocomplete.cpp
@@ -1,22 +1,22 @@
 #include "Autocomplete.h"
-std::vector<std::string> Autocomplete::getSuggestions(std::string partialWord)
-{std::vector<std::string> result; 
-for (int i = 0; i < words.size(); i++) {
-	int j = 0;
-	if (partialWord.length() <= words[i].size()) {
-		while (j <= partialWord.length() - 1 && words[i][j] == partialWord[j]) {
-			if (j >= partialWord.length() - 1) {
-				result.add(words[i]);
-			}
-			j++;
-		}
-	}
-}
-			return result;
+
+std::vector<std::string> Autocomplete::getSuggestions(std::string partialWord) {
+    std::vector<std::string> result;
+    for (int i = 0; i < words.size(); i++) {
+        int j = 0;
+        if (partialWord.length() <= words[i].size()) {
+            while (j <= partialWord.length() - 1 && words[i][j] == partialWord[j]) {
+                if (j >= partialWord.length() - 1) {
+                    result.push_back(words[i]);
+                }
+                j++;
+            }
+        }
+    }
+    return result;
 }
 
-void Autocomplete::insert(std::string word)
-{
-	words.add(word);
-	return;
+void Autocomplete::insert(std::string word) {
+    words.push_back(word);
+    return;
 }
--- PrefixMatcher.cpp
+++ PrefixMatcher.cpp
@@ -1,28 +1,29 @@
-#include"PrefixMatcher.h"
+#include <algorithm>
+#include "PrefixMatcher.h"
+
 int PrefixMatcher::selectRouter(std::string networkAddress) {
-	int longest_match = 0;
-	int routerNumber = 0;
-	for (int i =0 ; i < addresses.size(); i++) {
-		int min = min(networkAddress.length(), addresses.size());
-		int match_length =0;
-		for (int j = 0; j < min; j++)
-		{
-			if (addresses[i][j] == networkAddress[j]) {
-				match_length++;
-			}
-			else {
-				i = min;
-			}
-		}
-		if (match_length > longest_match) {
-			longest_match = match_length;
-			routerNumber = i;
-		}
-	}
-	return routerNumber;
+    int longest_match = 0;
+    int routerNumber = 0;
+    for (int i = 0; i < addresses.size(); i++) {
+        int min_length = std::min(networkAddress.length(), addresses[i].length());
+        int match_length = 0;
+        for (int j = 0; j < min_length; j++) {
+            if (addresses[i][j] == networkAddress[j]) {
+                match_length++;
+            }
+            else {
+                break;
+            }
+        }
+        if (match_length > longest_match) {
+            longest_match = match_length;
+            routerNumber = i;
+        }
+    }
+    return routerNumber;
 }
-void PrefixMatcher::insert(std::string address, int routerNumber)
-{
-	addresses.add(address);
-	numbers.add(routerNumber);
+
+void PrefixMatcher::insert(std::string address, int routerNumber) {
+    addresses.push_back(address);
+    numbers.push_back(routerNumber);
 }
